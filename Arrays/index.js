2e93ee26d805c6718bff2cdd504f364f58b051a0
Shubham Deshmukh <shubhamdeshm37@gmail.com>
2024-02-02 12:10:40 +0530

    Arrays Methods

--- Arrays/index.js
+++ Arrays/index.js
@@ -53,7 +53,15 @@ console.log(a.concat(b, c));
 
 console.log(c.sort());
 
-// Splice
+/*
+splice() Method:
+
+splice is used to change the contents of an array by removing or replacing existing elements and/or adding new elements in place.
+
+It takes multiple parameters, including the starting index, the number of elements to remove, and optional elements to add.
+
+The original array is modified.
+*/
 
 let number = [1, 2, 3, 4, 5, 6];
 
@@ -66,5 +74,24 @@ console.log(
   `Splice :: start - ${start} & end - ${end} : ${number.splice(start, end)}`
 );
 
-//Remaining Elements after slice
+//Remaining Elements after splice
 console.log(`After Splice : ${number}`);
+
+/*
+slice() Method:
+
+slice is used to create a new array by extracting a portion of an existing array.
+
+It takes two parameters: the starting index (inclusive) and the ending index (exclusive).
+
+The original array remains unchanged.
+*/
+const originalArray = [1, 2, 3, 4, 5];
+const slicedArray = originalArray.slice(1, 4);
+console.log(slicedArray);
+console.log(originalArray);
+
+// reverse
+const array = [3, 4, 5, 7, 8, 2, 9];
+console.log(`Original Array - ${array}`);
+console.log(`Reversed Array - ${array.reverse()}`);
